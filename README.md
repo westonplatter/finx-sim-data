50711edce0f7fda0930d2b7a25f29c453c2e813b
westonplatter <westonplatter@gmail.com>
2021-12-22 23:33:27 -0700

    start

---
+++ README.md
@@ -0,0 +1,9 @@
+# Finx Simulation Data
+
+## Install
+
+## How to use
+
+## License
+See LICENSE file
+
---
+++ generate_data.py
@@ -0,0 +1,7 @@
+from decimal import Decimal
+
+import pandas as pd
+from scipy.stats import norm
+
+
+def instrument_data(s: float, days: int, vol: Decimal):
